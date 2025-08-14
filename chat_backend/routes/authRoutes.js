51557641d127a00bf12a7c7dcf58af135a89cbc9
Rijul-Sharma <rijul.sharma117@gmail.com>
2025-08-14 13:54:53 +0530

    repo structure fixes

--- chat_backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { Signup, Login } from '../controllers/authController.js'
-
-const router = express.Router()
-
-router.post('/signup', Signup)
-router.post('/login', Login)
-
-export default router
--- chat_backend/routes/messageRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { getAllMessages, saveMessage } from '../controllers/messageController.js'
-
-const router = express.Router()
-
-router.get('/', getAllMessages)
-router.post('/save', saveMessage)
-    
-export default router
